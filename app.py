d6381f9cf2cdf12e172a6cc61fdd6951ff2e23ec
craigryy <harriajames97@gmail.com>
2023-05-22 23:06:12 +0100

    added a loggin

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import datetime
 from functools import wraps
+from app.models import User,BookCategory,BookModel
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Favour98@localhost/milo'
@@ -13,69 +14,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-
-    """User model for authentication."""
-
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.Integer(),unique=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=True)
-    LoggedIn = db.Column(db.Boolean, default=True)
-    Book = db.relationship('BookModel', backref='owner', lazy='dynamic')
-
-    def json(self):
-        '''Display output as json object.'''
-
-        return {id: self.id, 'username': self.username,
-                'password': self.password, 'LoggedIn': self.LoggedIn}
-
-
-    def __repr__(self):
-        return "<User(username='%s', email='%s')>" % (self.username,
-                                                      self.email)
-class BookCategory(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    created_by = db.Column(db.String(64))
-    books = db.relationship('BookModel', backref='bookModel', cascade="all, delete-orphan", lazy='dynamic')
-
-    def json(self):
-        ''' Display output as JSON object.'''
-
-        return {
-            "id": self.id,
-            "name": self.name,
-            "items_count": len(self.items.all()),
-            "created_at": self.date_created.strftime("%Y-%m-%d %H:%M:%S"),
-            "updated_at": self.date_modified.strftime("%Y-%m-%d %H:%M:%S"),
-            "created_by": self.created_by,
-        }
-
-
-class BookModel(db.Model):
-
-    """BookModel defined."""
-
-    __tablename__ = 'BookModels'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    done = db.Column(db.Boolean, default=False, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('book_category.id'), nullable=False)
-    category = db.relationship('BookCategory', backref=db.backref('books', lazy=True))
-
-    def json(self):
-        ''' Display output as JSON object.'''
-
-        return {
-            "id": self.id,
-            "title": self.title,
-            "created_at": self.date_created.strftime("%Y-%m-%d %H:%M:%S"),
-            "updated_at": self.date_modified.strftime("%Y-%m-%d %H:%M:%S"),
-            "done": self.done,
-        }
 
 #create a test route
 @app.route('/test', methods=['GET'])
@@ -83,7 +21,7 @@ def test():
   return make_response(jsonify({'message': 'test route'}), 200)
 
 
-
+#create a token
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -175,6 +113,25 @@ def delete_user(id):
   except e:
     return make_response(jsonify({'message': 'error deleting user'}), 500)
 
+#create a loggin
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
 
 @app.route('/categories/', methods=['POST'])
 @token_required
