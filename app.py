7c40a123bcd312ba12f97a450a1762cdc528e266
craigryy <harriajames97@gmail.com>
2023-05-23 11:16:32 +0100

    implemented another database

--- app.py
+++ app.py
@@ -5,17 +5,89 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import datetime
 from functools import wraps
-from app.models import User,BookCategory,BookModel
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Favour98@localhost/milo'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Favour98@localhost/milk'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+app.config['SECRET_KEY']='jesusislord'
 
 db = SQLAlchemy(app)
 
 
+class User(db.Model):
 
-#create a test route
+    """User model for authentication."""
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(100), unique=True)
+    name = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    admin = db.Column(db.Boolean)
+    Book= db.relationship('BookModel', backref='user', lazy='dynamic')
+
+
+
+    # def json(self):
+    #     '''Display output as json object.'''
+
+    #     return {id: self.id, 'name': self.name,
+    #             'password': self.password, 'admin': self.admin}
+
+
+    # def __repr__(self):
+    #     return "<User(name='%s', email='%s')>" % (self.name,
+    #                                                   self.email)
+class BookCategory(db.Model):
+    """BookCtegory table defined """
+
+    __tablename__='BookCategorys'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    created_by = db.Column(db.String(100))
+    books = db.relationship('BookModel', backref='category',primaryjoin='BookCategory.id == BookModel.category_id',cascade="all, delete-orphan", lazy='dynamic')
+    
+
+    # def json(self):
+    #     ''' Display output as JSON object.'''
+
+    #     return {
+    #         "id": self.id,
+    #         "name": self.name,
+    #         "items_count": len(self.items.all()),
+    #         "created_at": self.date_created.strftime("%Y-%m-%d %H:%M:%S"),
+    #         "updated_at": self.date_modified.strftime("%Y-%m-%d %H:%M:%S"),
+    #         "created_by": self.created_by,
+    #     }
+
+
+class BookModel(db.Model):
+
+    """BookModel defined."""
+
+    __tablename__ = 'BookModels'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean, default=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('BookCategorys.id'))
+
+
+    
+
+
+    # def json(self):
+    #     ''' Display output as JSON object.'''
+
+    #     return {
+    #         "id": self.id,
+    #         "title": self.title,
+    #         "created_at": self.date_created.strftime("%Y-%m-%d %H:%M:%S"),
+    #         "updated_at": self.date_modified.strftime("%Y-%m-%d %H:%M:%S"),
+    #         "done": self.done,
+    #     }
+
+#create a test routespip
 @app.route('/test', methods=['GET'])
 def test():
   return make_response(jsonify({'message': 'test route'}), 200)
@@ -45,11 +117,7 @@ def token_required(f):
 
 # create a user
 @app.route('/user', methods=['GET'])
-@token_required
-def get_all_users(current_user):
-
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
+def get_all_users():
 
     users = User.query.all()
 
@@ -60,18 +128,14 @@ def get_all_users(current_user):
         user_data['public_id'] = user.public_id
         user_data['name'] = user.name
         user_data['password'] = user.password
-        user_data['loggedIn'] = user.loggedIn
+        user_data['admin'] = user.admin
         output.append(user_data)
 
     return jsonify({'users' : output})
 
 
 @app.route('/user', methods=['POST'])
-@token_required
-def create_user(current_user):
-    if not current_user.admin:
-        return jsonify({'message' : 'Cannot perform that function!'})
-
+def create_user():
     data = request.get_json()
 
     hashed_password = generate_password_hash(data['password'], method='sha256')
@@ -79,8 +143,9 @@ def create_user(current_user):
     new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
     db.session.add(new_user)
     db.session.commit()
-
     return jsonify({'message' : 'New user created!'})
+    # except Exception as e :
+    #    return jsonify({'message':'otilo'})
 
 # get a user by id
 @app.route('/users/<int:id>', methods=['GET'])
@@ -89,8 +154,8 @@ def get_user(id):
     user = User.query.filter_by(id=id).first()
     if user:
       return make_response(jsonify({'user': user.json()}), 200)
-    return make_response(jsonify({'message': 'user not found'}), 404)
-  except e:
+    return make_response(jsonify({'message': 'user not found'}), 404
+except e:
     return make_response(jsonify({'message': 'error getting user'}), 500)
 
 # update a user
@@ -100,7 +165,7 @@ def update_user(id):
     user = User.query.filter_by(id=id).first()
     if user:
       data = request.get_json()
-      user.username = data['username']
+      user.rname = data['name']
       user.email = data['email']
       db.session.commit()
       return make_response(jsonify({'message': 'user updated'}), 200)
@@ -126,10 +191,10 @@ def delete_user(id):
 def login():
     auth = request.authorization
 
-    if not auth or not auth.username or not auth.password:
+    if not auth or not auth.name or not auth.password:
         return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
 
-    user = User.query.filter_by(name=auth.username).first()
+    user = User.query.filter_by(name=auth.name).first()
 
     if not user:
         return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
