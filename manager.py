0bac5079eea719e64334c8d76103f72311a64d85
craigryy <harriajames97@gmail.com>
2023-06-18 16:11:21 +0100

    updated Dockerfile

--- manager.py
+++ manager.py
@@ -2,4 +2,5 @@ from flask_app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run()
+    # Run the Flask app on host 0.0.0.0 and port 5000
+    app.run(host='0.0.0.0', port=5000)
