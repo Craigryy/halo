15eaadfa753af659ecd2a94ec56e40581e081cd2
James Harrison <116971272+Craigryy@users.noreply.github.com>
2023-07-21 19:48:11 +0100

    Update model.py using black and isort

--- flask_app/model.py
+++ flask_app/model.py
@@ -1,11 +1,10 @@
-'''Models defined for APi  '''
+"""Class decorator defined for the Model Scripts."""
 
 from config import db
 
 
 class Base(db.Model):
-
-    ''' Abstract Base class used to define id.'''
+    """Abstract Base class used to define id."""
 
     __abstract__ = True
 
@@ -24,45 +23,50 @@ class Base(db.Model):
 
 
 class User(Base):
-
     """User model for authentication."""
 
-    __tablename__ = 'user'
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(100), unique=True)
     name = db.Column(db.String(100))
     password = db.Column(db.String(100))
     admin = db.Column(db.Boolean)
-    category = db.relationship('BookCategory', backref='owner', lazy='dynamic')
+    category = db.relationship("BookCategory", backref="owner", lazy="dynamic")
 
     def to_json(self):
-        return {'id': self.id, 'name': self.name, 'public_id': self.public_id, 'password': self.password, 'admin': self.admin}
+        return {
+            "id": self.id,
+            "name": self.name,
+            "public_id": self.public_id,
+            "password": self.password,
+            "admin": self.admin,
+        }
 
 
 class BookCategory(Base):
-    """BookCategory table defined """
+    """BookCategory table defined"""
 
-    __tablename__ = 'BookCategorys'
+    __tablename__ = "BookCategorys"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     created_by = db.Column(db.String(100))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    books = db.relationship('BookModel', backref='category',
-                            cascade="all, delete-orphan", lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    books = db.relationship(
+        "BookModel", backref="category", cascade="all, delete-orphan", lazy="dynamic"
+    )
 
     def to_json(self):
-        return {'id': self.id, 'name': self.name, 'created_by': self.created_by}
+        return {"id": self.id, "name": self.name, "created_by": self.created_by}
 
 
 class BookModel(Base):
-
     """BookModel defined."""
 
-    __tablename__ = 'BookModels'
+    __tablename__ = "BookModels"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    author = db.Column(db.String(100)) 
-    category_id = db.Column(db.Integer, db.ForeignKey('BookCategorys.id'))
+    author = db.Column(db.String(100))
+    category_id = db.Column(db.Integer, db.ForeignKey("BookCategorys.id"))
 
     def to_json(self):
-        return {'id': self.id, 'title': self.title, 'author': self.author}
+        return {"id": self.id, "title": self.title, "author": self.author}
