3c5ec8c24abaef080ca0419d8eeb024d7ea8804b
Craigryy <harriajames97@gmail.com>
2023-09-10 23:09:35 +0100

    updated bookmodel.py

--- flask_app/bookmodel.py
+++ flask_app/bookmodel.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, make_response, request
-from flask_jwt_extended import jwt_required ,get_jwt_identity
-from .model import BookCategory, BookModel, User
+from flask_jwt_extended import jwt_required
+from .model import BookCategory, BookModel
 
 book_app = Blueprint('book_app', __name__)
 
@@ -21,7 +21,6 @@ def create_book(id):
     book.save()
 
     return jsonify({'book': book.to_json()})
-
 
 @book_app.route('/categories/<int:id>/books/', methods=['GET'])
 @jwt_required()
@@ -52,7 +51,6 @@ def get_books_by_category(id):
         error_message = f"Error fetching books: {str(e)}"
         return make_response(jsonify({'message': error_message}), 500)
 
-
 @book_app.route('/categories/<int:category_id>/books/<int:book_id>', methods=['GET'])
 @jwt_required()
 def get_book_and_category(category_id, book_id):
@@ -76,81 +74,37 @@ def get_book_and_category(category_id, book_id):
 
     return jsonify({'book': book_data})
 
-
 @book_app.route('/books/', methods=['GET'])
 @jwt_required()
-def get_books_by_user():
+def get_all_books():
     """
-    Get books owned by the currently authenticated user.
+    Get all books.
     """
     try:
-        # Get the user ID of the currently authenticated user
-        current_user_id = get_jwt_identity()
+        books = BookModel.query.all()
+        books_data = []
 
-        # Query the currently logged-in user based on their public_id
-        current_user = User.query.filter_by(public_id=current_user_id).first()
+        for book in books:
+            category = BookCategory.query.get(book.category_id)
+            if not category:
+                category_name = None
+            else:
+                category_name = category.name
 
-        if current_user:
-            # Retrieve only the books owned by the current user
-            books = BookModel.query.filter_by(user_id=current_user.id).all()
-            books_data = []
+            book_data = {
+                'id': book.id,
+                'title': book.title,
+                'author': book.author,
+                'category_id': book.category_id,
+                'category_name': category_name,
+            }
+            books_data.append(book_data)
 
-            for book in books:
-                category = BookCategory.query.get(book.category_id)
-                if not category:
-                    category_name = None
-                else:
-                    category_name = category.name
-
-                book_data = {
-                    'id': book.id,
-                    'title': book.title,
-                    'author': book.author,
-                    'category_id': book.category_id,
-                    'category_name': category_name,
-                }
-                books_data.append(book_data)
-
-            return jsonify(books_data)
-        else:
-            return make_response(jsonify({'message': 'User not found'}), 404)
+        return jsonify(books_data)
 
     except Exception as e:
         error_message = f"Error fetching books: {str(e)}"
         return make_response(jsonify({'message': error_message}), 500)
-
-
-# @book_app.route('/books/', methods=['GET'])
-# @jwt_required()
-# def get_all_books():
-#     """
-#     Get all books.
-#     """
-#     try:
-#         books = BookModel.query.all()
-#         books_data = []
-
-#         for book in books:
-#             category = BookCategory.query.get(book.category_id)
-#             if not category:
-#                 category_name = None
-#             else:
-#                 category_name = category.name
-
-#             book_data = {
-#                 'id': book.id,
-#                 'title': book.title,
-#                 'author': book.author,
-#                 'category_id': book.category_id,
-#                 'category_name': category_name,
-#             }
-#             books_data.append(book_data)
-
-#         return jsonify(books_data)
-
-#     except Exception as e:
-#         error_message = f"Error fetching books: {str(e)}"
-#         return make_response(jsonify({'message': error_message}), 500)
 
 @book_app.route('/categories/<int:id>/books/<int:book_id>/', methods=['PUT'])
 @jwt_required()
