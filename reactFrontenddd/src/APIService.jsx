c804f7b9c091c4a3567d71c5a72ee6924db1112f
Craigryy <harriajames97@gmail.com>
2023-09-13 11:40:37 +0100

    updated my app.js and APIService.jsx to make use of makeRequest

--- reactFrontenddd/src/APIService.jsx
+++ reactFrontenddd/src/APIService.jsx
@@ -7,7 +7,6 @@ export default class APIService {
     if (token) {
       headers["Authorization"] = `Bearer ${token}`;
     }
-
     const requestOptions = {
       method,
       headers,
@@ -17,75 +16,245 @@ export default class APIService {
       requestOptions.body = JSON.stringify(body);
     }
 
-    try {
-      const response = await fetch(url, requestOptions);
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status} ${response.statusText}`);
-      }
-      return await response.json();
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
-    }
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
+    return data;
   }
 
-  // Categories Routes
+  // get all categories from the server and store them in state
 
   static async insertCategory(body, token) {
-    return this.makeRequest(`/categories/add`, 'POST', body, token);
+    return fetch(`/categories/add`, {
+      method: "POST",
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error(`Error: ${resp.status} ${resp.statusText}`);
+      }
+      return resp.json();
+    })
+    .catch(error => {
+      console.error('Error inserting category:', error);
+    });
   }
 
-  static async UpdateCategory(categoryId, updatedData, token) {
-    return this.makeRequest(`/categories/${categoryId}/`, 'PUT', updatedData, token);
-  }
+  static async UpdateCategory (categoryId, updatedData, token) {
+    return fetch(`/categories/${categoryId}/`, {
+        method: 'PUT', 
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(updatedData)
+      })
+      .then(resp => {
+        if (!resp.ok) {
+          throw new Error(`Error: ${resp.status} ${resp.statusText}`);
+        }
+        return resp.json();
+      })
+      .catch(error => {
+        console.error('Error inserting category:', error);
+      });
+    }
 
-  static async DeleteCategory(categoryId, token) {
-    return this.makeRequest(`/categories/${categoryId}/`, 'DELETE', null, token);
-  }
+   static async DeleteCategory (categoryId, token)  {
+    return fetch(`/categories/${categoryId}/`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to delete category');
+      }
+    })
+    .catch(error => {
+      throw error;
+    });
+}
 
-  // Books Routes
 
-  static async addBook(categoryId, body, token) {
-    return this.makeRequest(`/categories/${categoryId}/books/`, 'POST', body, token);
+  
+  //  BOOKS ROUTES
+
+  static async addBook(id,body, token) {
+    return fetch(`/categories/${id}/books/`, {
+      method: "POST",
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error(`Error: ${resp.status} ${resp.statusText}`);
+      }
+      return resp.json();
+    })
+    .catch(error => {
+      console.error('Error adding book:', error);
+      console.error('Response body:', error.response);
+    });
   }
 
   static async getAllBooks(token) {
-    return this.makeRequest('/books/', 'GET', null, token);
+    return fetch('/books/', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error(`Error: ${resp.status} ${resp.statusText}`);
+      }
+      return resp.json();
+    })
+    .catch(error => {
+      console.error('Error fetching all books:', error);
+    });
   }
+  
 
-  static async updateBook(categoryId, bookId, updatedData, token) {
-    return this.makeRequest(`/categories/${categoryId}/books/${bookId}`, 'PUT', updatedData, token);
-  }
+  static async updateBook (categoryId, bookId,updatedData, token) {
+    return fetch(`/categories/${categoryId}/books/${bookId}`, {
+        method: 'PUT', 
+        headers: {
+          'Content-Type': 'application/json',
+         'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(updatedData)
+      })
+      .then(resp => {
+        if (!resp.ok) {
+          throw new Error(`Error: ${resp.status} ${resp.statusText}`);
+        }
+        return resp.json();
+      })
+      .catch(error => {
+        console.error('Error inserting book:', error);
+      });
+    }
 
-  static async deleteBook(categoryId, bookId, token) {
-    return this.makeRequest(`/categories/${categoryId}/books/${bookId}`, 'DELETE', null, token);
-  }
 
-  // Login Routes
 
-  static async login(username, password) {
-    const url = '/login';
-    const body = { name: username, password };
+  static async deleteBook (categoryId,bookId, token)  {
+    return fetch(`/categories/${categoryId}/books/${bookId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to delete book');
+      }
+    })
+    .catch(error => {
+      throw error;
+    });
+  }   
+  
+  //  Login Routes
 
-    return this.makeRequest(url, 'POST', body);
-  }
+static async login(username, password) {
+  const url = '/login';
+  const body = { name: username, password };
 
-  // Users Routes
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Login failed. Please check your credentials and try again.');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const token = data.access_token;
+    return token;
+  })
+  .catch(error => {
+    console.error('Error fetching token:', error);
+    throw new Error('Login failed. Please check your credentials and try again.');
+  });
+}
+
+  // / Users Routes
 
   static async createUser(username, password) {
     const requestBody = {
       name: username,
       password: password
     };
-
-    return this.makeRequest('/auth/register', 'POST', requestBody);
+  
+    return fetch('/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestBody)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error('Error creating user:', error);
+      });
   }
+  
 
   static async getAllUsers(token) {
-    return this.makeRequest('/users', 'GET', null, token);
+    return fetch("/users", {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error(`Error: ${resp.status} ${resp.statusText}`);
+      }
+      return resp.json();
+    })
+    .catch(error => {
+      console.error('Error fetching all users:', error);
+    });
   }
-
-  static async deleteUser(id, token) {
-    return this.makeRequest(`/user/${id}/`, 'DELETE', null, token);
-  }
+  static async deleteUser (Id, token)  {
+    return fetch(`/user/${Id}/`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to delete a user');
+      }
+    })
+    .catch(error => {
+      throw error;
+    });
+  } 
 }
